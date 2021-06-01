e6e3b61697e55ad26eae3c5f32cc99c6bf76705a
Colin Edwards <ColinJhEdwards@gmail.com>
2021-05-31 19:05:40 -0700

    created a input bar that when typed in will filter the employees based off the value

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,40 +5,81 @@ import styled from "styled-components";
 
 const UserList = () => {
   const [userData, setUserData] = useState([]);
+  const [searchData, setSearchData] = useState([]);
   useEffect(() => {
     axios
       .get("https://randomuser.me/api/?results=20")
       .then((data) => {
         setUserData(data.data.results);
+        setSearchData(data.data.results);
       })
       .catch((err) => console.log(err));
   }, []);
-  console.log("UserData", userData);
+
+  const handleSearch = (e) => {
+    let value = e.target.value.toLowerCase();
+    let result = [];
+    result = userData.filter((data) => {
+      return (
+        `${data.name.first.toLowerCase()} ${data.name.last.toLowerCase()}`.search(
+          value
+        ) != -1
+      );
+    });
+    if (value === "") {
+      setUserData(searchData);
+    } else {
+      setUserData(result);
+    }
+  };
+
   return (
-    <StyledList>
-      {userData.map((user) => (
-        <UserCard
-          cell={user.cell}
-          age={user.dob.age}
-          email={user.email}
-          gender={user.gender}
-          location={`${user.location.city}, ${user.location.country}`}
-          name={`${user.name.first} ${user.name.last}`}
-          picture={user.picture.large}
-          hired={user.registered.date}
-          timeAtCompany={user.registered.age}
+    <div>
+      <StyledInput>
+        <input
+          onChange={(e) => handleSearch(e)}
+          type="text"
+          placeholder="Search"
         />
-      ))}
-    </StyledList>
+      </StyledInput>
+      <StyledList>
+        {userData.map((user) => (
+          <UserCard
+            cell={user.cell}
+            age={user.dob.age}
+            email={user.email}
+            gender={user.gender}
+            location={`${user.location.city}, ${user.location.country}`}
+            name={`${user.name.first} ${user.name.last}`}
+            picture={user.picture.large}
+            hired={user.registered.date}
+            timeAtCompany={user.registered.age}
+          />
+        ))}
+      </StyledList>
+    </div>
   );
 };
 
 const StyledList = styled.div`
+  margin-top: 2rem;
   min-height: 90vh;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+`;
+
+const StyledInput = styled.div`
+  margin-top: 2rem;
+  min-height: 5vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  input {
+    font-size: 2rem;
+    font-family: "Roboto", sans-serif;
+  }
 `;
 
 export default UserList;
